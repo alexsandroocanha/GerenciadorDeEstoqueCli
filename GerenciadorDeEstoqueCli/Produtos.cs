2bc565abb24ea715387514cc1d6aef2871046028
Alexsandro Ocanha <alexsandroocanha@gmail.com>
2025-11-17 13:50:23 -0300

    feat: Initial 🎉

---
+++ GerenciadorDeEstoqueCli/Produtos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace GerenciadorDeEstoqueCli
+{
+    internal class Produtos
+    {
+        public string Nome;
+        public double Preco;
+        public int Estoque;
+
+        public void AdicionarProduto()
+        {
+            Console.WriteLine("Insira o Nome do Produto");
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o Preco do Produto");
+            Console.Write("R$ ");
+            Preco = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Insira a Quantidade do Produto");
+            Estoque += int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Preço total: R$" + TotalPrecoProduto());
+        }
+
+        public void RemoverProduto()
+        {
+            Console.WriteLine("Insira a Quantidade que deseja remover do Produto");
+            Estoque -= int.Parse(Console.ReadLine());
+        }
+
+        public void ListarProduto()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Preco: R$" + Preco);
+            Console.WriteLine("Quantidade: " + Estoque);
+            Console.WriteLine("Total: R$" + TotalPrecoProduto());
+        }
+
+        private double TotalPrecoProduto()
+        {
+            return Estoque * Preco;
+        }
+
+    }
+}
--- GerenciadorDeEstoqueCli/Program.cs
+++ GerenciadorDeEstoqueCli/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+namespace GerenciadorDeEstoqueCli
+{
+    class Program
+    {
+        static void Main()
+        {
+            Produtos produto;
+            produto = new Produtos();
+
+            bool saidaDoLaco = true;
+            
+            while (saidaDoLaco)
+            {
+
+
+                Console.WriteLine("-------- Digite uma opção --------");
+                Console.WriteLine("[1] Adicionar produto:");
+                Console.WriteLine("[2] Remover produto:");
+                Console.WriteLine("[3] Listar produtos:");
+                Console.WriteLine("[4] Sair do Programa:");
+
+                int respostaUsuario = int.Parse(Console.ReadLine());
+
+                switch (respostaUsuario)
+                {
+                    case 1:
+                        produto.AdicionarProduto();
+                        break;
+                    case 2:
+                        produto.RemoverProduto();
+                        break;
+                    case 3:
+                        produto.ListarProduto();
+                        break;
+                    case 4:
+                        saidaDoLaco = false;
+                        break;
+                }
+            }
+        }
+    }
+}
